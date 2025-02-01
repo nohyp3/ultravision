bfd60a9091d5abf25dbd8cbd09d2a6e242459cc0
Ramadan Haider <120184942+ramadanhaider@users.noreply.github.com>
2025-02-01 18:22:23 -0500

    object detection base

---
+++ ultrasound.ino
@@ -0,0 +1,29 @@
+#define TRIG_PIN 5
+#define ECHO_PIN 18
+
+void setup() {
+    Serial.begin(115200);
+    pinMode(TRIG_PIN, OUTPUT);
+    pinMode(ECHO_PIN, INPUT);
+}
+
+void loop() {
+    digitalWrite(TRIG_PIN, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG_PIN, LOW);
+
+    long duration = pulseIn(ECHO_PIN, HIGH);
+    float distance = duration * 0.034 / 2;
+
+    Serial.print("Distance: ");
+    Serial.print(distance);
+    Serial.println(" cm");
+
+    if (distance < 10) {
+        Serial.println("Object detected!");
+    }
+
+    delay(500);
+}
